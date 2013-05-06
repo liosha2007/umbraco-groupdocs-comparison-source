ea092b707a5c662cd092de4ba293c42b9bf2499e
aleksey.permyakov <aleksey.permyakov@groupdocs.com>
2013-05-06 09:17:10 +0300

    Initial commit: Added base plugin functional

---
+++ ClassLibrary1/EmbedComparison.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Groupdocs.EmbedComparison
+{
+    public class EmbedComparison : System.Web.UI.UserControl
+    {
+        public String CompareGuid { get; set; }
+        public String CompareKey { get; set; }
+        public int FrameWidth { get; set; }
+        public int FrameHeight { get; set; }
+    }
+}
---
+++ Web/Views/EmbedComparison.ascx
@@ -0,0 +1,3 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" Inherits="Groupdocs.EmbedComparison.EmbedComparison" %>
+
+<iframe src="http://apps.groupdocs.com/document-comparison/embed/<%= CompareKey %>/<%= CompareGuid %>?referer=Umbraco/1.0" frameborder="0" width="<%= FrameWidth %>" height="<%= FrameHeight %>"></iframe>
